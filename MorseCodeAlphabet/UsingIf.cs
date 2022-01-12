fdd2fb0dd767d6cce486b90a02c511da93eab83d
a91n0d <a91n0d@gmail.com>
2022-01-12 09:27:04 +0300

    Add project files.

---
+++ MorseCodeAlphabet/UsingIf.cs
@@ -0,0 +1,117 @@
+﻿#pragma warning disable S3776
+
+namespace MorseCodeAlphabet
+{
+    public static class UsingIf
+    {
+        public static string GetMorseCode(char c)
+        {
+            if (char.ToUpperInvariant(c) == 'A')
+            {
+                return ".-";
+            }
+            else if (char.ToUpperInvariant(c) == 'B')
+            {
+                return "-...";
+            }
+            else if (char.ToUpperInvariant(c) == 'C')
+            {
+                return "-.-.";
+            }
+            else if (char.ToUpperInvariant(c) == 'D')
+            {
+                return "-..";
+            }
+            else if (char.ToUpperInvariant(c) == 'E')
+            {
+                return ".";
+            }
+            else if (char.ToUpperInvariant(c) == 'F')
+            {
+                return "..-.";
+            }
+            else if (char.ToUpperInvariant(c) == 'G')
+            {
+                return "--.";
+            }
+            else if (char.ToUpperInvariant(c) == 'H')
+            {
+                return "....";
+            }
+            else if (char.ToUpperInvariant(c) == 'I')
+            {
+                return "..";
+            }
+            else if (char.ToUpperInvariant(c) == 'J')
+            {
+                return ".---";
+            }
+            else if (char.ToUpperInvariant(c) == 'K')
+            {
+                return "-.-";
+            }
+            else if (char.ToUpperInvariant(c) == 'L')
+            {
+                return ".-..";
+            }
+            else if (char.ToUpperInvariant(c) == 'M')
+            {
+                return "--";
+            }
+            else if (char.ToUpperInvariant(c) == 'N')
+            {
+                return "-.";
+            }
+            else if (char.ToUpperInvariant(c) == 'O')
+            {
+                return "---";
+            }
+            else if (char.ToUpperInvariant(c) == 'P')
+            {
+                return ".--.";
+            }
+            else if (char.ToUpperInvariant(c) == 'Q')
+            {
+                return "--.-";
+            }
+            else if (char.ToUpperInvariant(c) == 'R')
+            {
+                return ".-.";
+            }
+            else if (char.ToUpperInvariant(c) == 'S')
+            {
+                return "...";
+            }
+            else if (char.ToUpperInvariant(c) == 'T')
+            {
+                return "-";
+            }
+            else if (char.ToUpperInvariant(c) == 'U')
+            {
+                return "..-";
+            }
+            else if (char.ToUpperInvariant(c) == 'V')
+            {
+                return "...-";
+            }
+            else if (char.ToUpperInvariant(c) == 'W')
+            {
+                return ".--";
+            }
+            else if (char.ToUpperInvariant(c) == 'X')
+            {
+                return "-..-";
+            }
+            else if (char.ToUpperInvariant(c) == 'Y')
+            {
+                return "-.--";
+            }
+            else if (char.ToUpperInvariant(c) == 'Z')
+            {
+                return "--..";
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ MorseCodeAlphabet/UsingSwitch.cs
@@ -0,0 +1,67 @@
+﻿namespace MorseCodeAlphabet
+{
+    public static class UsingSwitch
+    {
+        public static byte GetMorseCode(char c)
+        {
+            switch (char.ToLowerInvariant(c))
+            {
+                case 'a':
+                    return 0b0011_0001;
+                case 'b':
+                    return 0b1111_1110;
+                case 'c':
+                    return 0b1111_1010;
+                case 'd':
+                    return 0b0111_0110;
+                case 'e':
+                    return 0b0001_0001;
+                case 'f':
+                    return 0b1111_1011;
+                case 'g':
+                    return 0b0111_0100;
+                case 'h':
+                    return 0b1111_1111;
+                case 'i':
+                    return 0b0011_0011;
+                case 'j':
+                    return 0b1111_0001;
+                case 'k':
+                    return 0b0111_0010;
+                case 'l':
+                    return 0b1111_1101;
+                case 'm':
+                    return 0b0011_0000;
+                case 'n':
+                    return 0b0011_0010;
+                case 'o':
+                    return 0b0111_0000;
+                case 'p':
+                    return 0b1111_1001;
+                case 'q':
+                    return 0b1111_0100;
+                case 'r':
+                    return 0b0111_0101;
+                case 's':
+                    return 0b0111_0111;
+                case 't':
+                    return 0b0001_0000;
+                case 'u':
+                    return 0b0111_0011;
+                case 'v':
+                    return 0b1111_0111;
+                case 'w':
+                    return 0b0111_0001;
+                case 'x':
+                    return 0b1111_0110;
+                case 'y':
+                    return 0b1111_0010;
+                case 'z':
+                    return 0b1111_1100;
+
+                default:
+                    return 0b0000_0000;
+            }
+        }
+    }
+}
